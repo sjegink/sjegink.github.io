69a4327f66eb17075f22cc1d76c71aaa74ee5ce3
sjegink <sjegink@gmail.com>
2024-06-26 18:11:51 +0900

    expr: ref asset path

--- .lib/exam/next.config.mjs
+++ .lib/exam/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
 	output: 'export',
+	assetPrefix: isProd ? 'https://sjegink.github.io/exam' : undefined,
 };
 
 export default nextConfig;
