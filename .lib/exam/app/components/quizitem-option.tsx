f19bfa6b2e6051279939b9db1bf0cb4073e90cb4
sjegink <sjegink@gmail.com>
2025-03-24 13:23:15 +0900

    Fix: optimize click boundary

--- .lib/exam/app/components/quizitem-option.tsx
+++ .lib/exam/app/components/quizitem-option.tsx
@@ -1,14 +1,18 @@
 import { HTMLAttributes, MouseEvent } from "react";
 import styled from "styled-components";
 
-export type QuizitemOptionProps = HTMLAttributes<HTMLLIElement> | string;
+export type QuizitemOptionProps = HTMLAttributes<HTMLAnchorElement> | string;
 
 export default function QuizitemOption(props: QuizitemOptionProps) {
 	if (typeof props === 'string') {
-		props = { children: props } as HTMLAttributes<HTMLLIElement>
+		props = { children: props } as HTMLAttributes<HTMLAnchorElement>
 	}
 
-	const template = <Styled_li {...props} onClick={onClick} />
+	const template = (
+		<Styled_li>
+			<a {...props} onClick={onClick} />
+		</Styled_li>
+	)
 
 	function onClick(ev: MouseEvent) {
 		const eOption = ev.currentTarget as HTMLLIElement;
@@ -30,35 +34,39 @@ export default function QuizitemOption(props: QuizitemOptionProps) {
 }
 
 const Styled_li = styled.li`
-	position: relative;
-	&::before {
-		margin-right: 0.25rem;
-	}
-	&:nth-child(1)::before { content: "①"; }
-	&:nth-child(2)::before { content: "②"; }
-	&:nth-child(3)::before { content: "③"; }
-	&:nth-child(4)::before { content: "④"; }
-	&:nth-child(5)::before { content: "⑤"; }
-
-	cursor: pointer;
-	&:hover {
-		-webkit-text-stroke: .5px blue;
-	}
-
-	&.selected{
-		color: blue;
-		-webkit-text-stroke: .5px black;
-		&:hover {
-			-webkit-text-stroke-color: darkblue;
+	> a {
+		position: relative;
+		&::before {
+			margin-right: 0.25rem;
 		}
-		&::after {
-			content: '';
-			position: absolute;
-			bottom: 0.4rem;
-			left: -0.1rem;
-			width: 1.5rem;
-			height: 1.5rem;
-			background: transparent url('check.svg') center center / contain no-repeat;
+	}
+	&:nth-child(1) > a::before { content: "①"; }
+	&:nth-child(2) > a::before { content: "②"; }
+	&:nth-child(3) > a::before { content: "③"; }
+	&:nth-child(4) > a::before { content: "④"; }
+	&:nth-child(5) > a::before { content: "⑤"; }
+
+	> a {
+		cursor: pointer;
+		&:hover {
+			-webkit-text-stroke: .5px blue;
+		}
+
+		&.selected{
+			color: blue;
+			-webkit-text-stroke: .5px black;
+			&:hover {
+				-webkit-text-stroke-color: darkblue;
+			}
+			&::after {
+				content: '';
+				position: absolute;
+				bottom: 0.4rem;
+				left: -0.1rem;
+				width: 1.5rem;
+				height: 1.5rem;
+				background: transparent url('check.svg') center center / contain no-repeat;
+			}
 		}
 	}
 `;
